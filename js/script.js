31e9760218a5a0c4d23c879e7d82dce9274c6738
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-08-10 12:20:52 +0500

    add edits

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 let elForm = document.querySelector(".shopping-form");
 let elInput = document.querySelector(".product-taker");
 let elList = document.querySelector(".shopping-list");
+let elTextAlready = document.querySelector(".text-for-already");
+let arrayList = [];
 
 elForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
   
-  let list = [];
   let elInputValue = elInput.value;
   elInput.value = "";
-  list.push(elInputValue);
 
-  for (item of list) {
-    let elItem = document.createElement("li");
-    elItem.textContent = item;
+  arrayList.push(elInputValue);
+  
+  let elItem = document.createElement("li");
+
+  for (item of arrayList) {
+    elItem.classList.add("product-item")
     elList.appendChild(elItem);
+    elItem.textContent = "";
+    elItem.textContent = item;
   }
+
+  elItem.addEventListener('click', function() {
+    elItem.style.textDecoration = "line-through";
+  });
+
+  elItem.addEventListener('dblclick', function(){
+    elList.removeChild(elItem);
+  });
 })
