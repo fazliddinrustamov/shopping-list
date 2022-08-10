feee14ee8c967c66748f0b27b8ce4f32a8b0f009
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-08-10 16:02:18 +0500

    add edits

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let elForm = document.querySelector(".shopping-form");
 let elInput = document.querySelector(".product-taker");
 let elList = document.querySelector(".shopping-list");
-let elTextAlready = document.querySelector(".text-for-already");
 let arrayList = [];
 
 elForm.addEventListener('submit', function (evt) {
@@ -9,23 +8,31 @@ elForm.addEventListener('submit', function (evt) {
   
   let elInputValue = elInput.value;
   elInput.value = "";
-
+  
   arrayList.push(elInputValue);
   
   let elItem = document.createElement("li");
 
   for (item of arrayList) {
-    elItem.classList.add("product-item")
     elList.appendChild(elItem);
     elItem.textContent = "";
     elItem.textContent = item;
   }
+  let elDeleteButton = document.createElement("button");
+  elDeleteButton.textContent = "Delete!";
+  elDeleteButton.classList.add("del-btn");
+  elItem.appendChild(elDeleteButton);
+  elItem.style.fontSize = "20px";
+  elItem.style.color = "rgb(59, 65, 60)";
+  elItem.style.cursor = "pointer";
+  elItem.style.fontWeight = "bold";
+  elItem.style.marginTop = "20px";
 
   elItem.addEventListener('click', function() {
-    elItem.style.textDecoration = "line-through";
+    elItem.classList.toggle("product-item");
   });
 
-  elItem.addEventListener('dblclick', function(){
+  elDeleteButton.addEventListener('click', function(){
     elList.removeChild(elItem);
   });
-})
+}) 
